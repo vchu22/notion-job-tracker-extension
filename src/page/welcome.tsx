b6bd371ee42728744f4064707c929b233cf9f914
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-08-09 19:00:50 -0400

    feat: add routing from WelcomePage to CredentialsInputPage

--- src/page/welcome.tsx
+++ src/page/welcome.tsx
@@ -1,18 +1,22 @@
 import React from "react";
+import { goTo } from 'react-chrome-extension-router';
+import CredentialsInputPage from './credentials-input';
 import { useAppContext } from "../app-context";
 import { saveCredentials, loadCredentials } from "../lib/storage";
 
 function WelcomePage() {
     const { setFirstTimeRun } = useAppContext()
+
     const handleClick = () => {
         setFirstTimeRun(false);
+        goTo(CredentialsInputPage);
     }
     return (
-        <form>
+        <>
             <h2>Welcome to Notion Job Tracker!</h2>
             <p>This is a Chrome extension that allows you to add job posts to your Notion and track your job applications using Notion databases.</p>
             <button onClick={handleClick}>Get Started</button>
-        </form>
+        </>
     )
 }
 
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Router } from 'react-chrome-extension-router';
 import CredentialsInputPage from './page/credentials-input'
 import WelcomePage from './page/welcome'
 
-import {AppProvider} from "./app-context";
+import { AppProvider, useAppContext } from "./app-context";
 import './tailwind.css';
 import './popup.css';
 
 function Popup() {
     return (
         <AppProvider>
-            <>
-                <WelcomePage/>
-                {/*<CredentialsInputPage></CredentialsInputPage>*/}
-            </>
+            <div className="content-wrapper">
+                <Router>
+                    <WelcomePage/>
+                </Router>
+            </div>
         </AppProvider>
     )
 }
+
 const container = document.getElementById('app')!;
 const root = createRoot(container);
 root.render(<Popup/>);
