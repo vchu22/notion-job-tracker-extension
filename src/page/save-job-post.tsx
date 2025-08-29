2f2d9f0fb2752543f379045dfd915b79aaefe1c4
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-08-29 18:24:03 -0400

    feat: automatically fill in URL field with current tab's URL

--- src/page/save-job-post.tsx
+++ src/page/save-job-post.tsx
@@ -8,7 +8,7 @@ const typeInputs = {
     "title": (name:string, values: {[key:string]: any}, setValues: any) => <input type="text" id={name} name={name} value={values[name]} onChange={(e) => setValues({...values, [name]: e.target.value})}/>,
     "date": (name:string, values: {[key:string]: any}, setValues: any) => <input type="date" id={name} name={name} value={values[name]} onChange={(e) => setValues({...values, [name]: e.target.value})}/>,
     "number": (name:string, values: {[key:string]: any}, setValues: any) => <input type="number" id={name} name={name} value={values[name]} onChange={(e) => setValues({...values, [name]: e.target.value})}/>,
-    "url": (name:string, values: {[key:string]: any}, setValues: any) => <input type="url" id={name} name={name} value={values[name]} onChange={(e) => setValues({...values, [name]: e.target.value})}/>,
+    "url": (name:string, values: {[key:string]: any}, setValues: any, tabURL?: string) => <input type="url" id={name} name={name} value={tabURL || values[name]} onChange={(e) => setValues({...values, [name]: e.target.value})}/>,
     "rich_text": (name:string, values: {[key:string]: any}, setValues: any) => <textarea id={name} name={name} value={values[name]} onChange={(e) => setValues({...values, [name]: e.target.value})}/>,
     "select": (name:string, opt: { options: Array<{ id: string, name: string, [key: string]: any } >
 }, values: {[key:string]: any}, setValues: any) => {
@@ -25,7 +25,13 @@ const typeInputs = {
 function SaveJobPost() {
     const {boardName, boardColumns} = useAppContext()
     const columnNames = Object.keys(boardColumns);
-    const [fieldValues, setFieldValues] = useState(Object.keys(boardColumns).reduce((acc, key) => ({...acc, [key]: ""}), {}))
+    const [activeTabURL, setActiveTabURL] = useState("")
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
+        if (tab && typeof tab.url === 'string') {
+            setActiveTabURL(tab.url)
+        }
+    });
+    const [fieldValues, setFieldValues] = useState(Object.keys(boardColumns).reduce((acc, key) => {return ({...acc, [key]: ""})}, {}))
 
     return (
         <>
@@ -38,19 +44,19 @@ function SaveJobPost() {
                 <table>
                     <tbody>
                     { columnNames.map(key => {
-                    // @ts-ignore
-                    const type: string = boardColumns[key]["type"]
-                    // @ts-ignore
-                    const elemFunc = typeInputs[type];
-                    if (elemFunc != undefined) {
                         // @ts-ignore
-                        const elem = type == "select" || type == "multi_select"? elemFunc(key, boardColumns[key]["select"] || boardColumns[key]["multi_select"], fieldValues, setFieldValues) : elemFunc(key, fieldValues, setFieldValues);
-                        return (
-                            <tr key={key}>
-                                <td>{key}</td>
-                                <td>{elem}</td>
-                            </tr>)
-                    }
+                        const type: string = boardColumns[key]["type"]
+                        // @ts-ignore
+                        const elemFunc = typeInputs[type];
+                        if (elemFunc != undefined) {
+                            // @ts-ignore
+                            const elem = type == "select" || type == "multi_select"? elemFunc(key, boardColumns[key]["select"] || boardColumns[key]["multi_select"], fieldValues, setFieldValues) : (type=="url"? elemFunc(key, fieldValues, setFieldValues, activeTabURL): elemFunc(key, fieldValues, setFieldValues));
+                            return (
+                                <tr key={key}>
+                                    <td>{key}</td>
+                                    <td>{elem}</td>
+                                </tr>)
+                        }
                 })}
                 </tbody>
                 <tfoot>
